c9db038d8213f69fde753747c87d9198fcb38ca6
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-05-12 16:16:34 -0300

    aula 04

---
+++ aula04/aula04.js
@@ -0,0 +1,9 @@
+var somaFunc = require('./somar.js');
+var subFunc = require('./sub.js');
+var multiFunc = require('./multi.js');
+var divFunc = require('./div.js');
+
+console.log(somaFunc(1,2));
+console.log(subFunc(10,5));
+console.log(multiFunc(2,15));
+console.log(divFunc(30,2));
