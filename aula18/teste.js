c82d2844b7c0682a0a0ca7d4e8f81d46c4ee3d49
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-05-14 13:25:41 -0300

    Aula 18

---
+++ aula18/teste.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('teste', 'root', 'sjmb434718', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
