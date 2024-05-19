6250dd6bf5fee473837f11314a21cb8111d061bd
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2024-05-19 15:47:14 -0300

    Aula 33

--- aula33/app.js
+++ aula33/app.js
@@ -4,20 +4,23 @@
     const bodyParser = require('body-parser');
     const app = express();
     const admin = require('./roots/admin.js');
+    const path = require('path');
     // const mongoose = require('mongoose');
-//Configurações
+// Configurações
     // Body-parser
         app.use(bodyParser.urlencoded({extended: true}));
         app.use(bodyParser.json());
-    //Handlebars
+    // Handlebars
         app.engine('handlebars',handlebars.engine({defaultLayout: 'main'}));
         app.set('view engine','handlebars');
     // Mongoose
         //em breve
+    // Public
+        app.use(express.static(path.join(__dirname,'public')));
     //
-//Rotas
+// Rotas
     app.use('/admin',admin);
-//outros
+// outros
 const PORT = 2024;
 app.listen(PORT,()=>{
     console.log('Servidor rodando...');
